35b223f4bf60c567191ab41bfb3a388f1ee32a3c
Владимир Терентьев <swatik07@yandex.ru>
2020-11-13 18:32:34 +0700

    Поменял кодировку на UNICODE

--- oop_prog/Test_for_1lab.h
+++ oop_prog/Test_for_1lab.h
@@ -1,44 +1,44 @@
-#include "Complex.h"
+ï»¿#include "Complex.h"
 #pragma once
 
 void Lab1_test1(bool result)
 {
 	cout << "===================Test #1===================\n";
 
-	// Ñîçäà¸ì îáúåêòû êîìïëåêñíîãî ÷èñëà.
-	Complex ex1_test1(5, 4, "(5 + 4i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex ex2_test1(11, 7, "(11 + 7i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex res_test1; // Âûçâàëñÿ ñòàíäàòðòíûé êîíñòðóêòîð.
+	// Ð¡Ð¾Ð·Ð´Ð°Ñ‘Ð¼ Ð¾Ð±ÑŠÐµÐºÑ‚Ñ‹ ÐºÐ¾Ð¼Ð¿Ð»ÐµÐºÑÐ½Ð¾Ð³Ð¾ Ñ‡Ð¸ÑÐ»Ð°.
+	Complex ex1_test1(5, 4, "(5 + 4i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex ex2_test1(11, 7, "(11 + 7i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex res_test1; // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÑÑ‚Ð°Ð½Ð´Ð°Ñ‚Ñ€Ñ‚Ð½Ñ‹Ð¹ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€.
 
-	// Ñëîæåíèå.
+	// Ð¡Ð»Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Summation---\n";
 	res_test1.summarize(ex1_test1, ex2_test1);
 	cout << ex1_test1.getExpression(); cout << " + "; cout << ex2_test1.getExpression(); cout << " = ";
 	res_test1.showResult();
 	res_test1.compare_with(16, 11);
 
-	// Âû÷èòàíèå.
+	// Ð’Ñ‹Ñ‡Ð¸Ñ‚Ð°Ð½Ð¸Ðµ.
 	cout << "---Subtraction---\n";
 	res_test1.subtract(ex1_test1, ex2_test1);
 	cout << ex1_test1.getExpression(); cout << " - "; cout << ex2_test1.getExpression(); cout << " = ";
 	res_test1.showResult();
 	res_test1.compare_with(-6, -3);
 
-	// Äåëåíèå.
+	// Ð”ÐµÐ»ÐµÐ½Ð¸Ðµ.
 	cout << "---Division---\n";
 	res_test1.divide(ex1_test1, ex2_test1);
 	cout << ex1_test1.getExpression(); cout << " / "; cout << ex2_test1.getExpression(); cout << " = ";
 	res_test1.showResult();
 	res_test1.compare_with(0.48823, 0.0529);
 
-	// Óìíîæåíèå.
+	// Ð£Ð¼Ð½Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Multiplication---\n";
 	res_test1.multiply(ex1_test1, ex2_test1);
 	cout << ex1_test1.getExpression(); cout << " * "; cout << ex2_test1.getExpression(); cout << " = ";
 	res_test1.showResult();
 	res_test1.compare_with(27, 79);
 
-	// Ñðàâíèâàíèå.
+	// Ð¡Ñ€Ð°Ð²Ð½Ð¸Ð²Ð°Ð½Ð¸Ðµ.
 	cout << "---Comparison---\n";
 	result = compare(ex1_test1, ex2_test1);
 	if (result)
@@ -52,46 +52,46 @@ void Lab1_test1(bool result)
 
 	res_test1.showNumber();
 
-} // âûçâàëîñü 3 äåñòðóêòîðà äëÿ îáúåêòîâ: ex1_test1 ; ex2_test1 ; res_test1 . Ïàìÿòü îñâîáîæäåíà.
+} // Ð²Ñ‹Ð·Ð²Ð°Ð»Ð¾ÑÑŒ 3 Ð´ÐµÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€Ð° Ð´Ð»Ñ Ð¾Ð±ÑŠÐµÐºÑ‚Ð¾Ð²: ex1_test1 ; ex2_test1 ; res_test1 . ÐŸÐ°Ð¼ÑÑ‚ÑŒ Ð¾ÑÐ²Ð¾Ð±Ð¾Ð¶Ð´ÐµÐ½Ð°.
 
 void Lab1_test2(bool result)
 {
 	cout << "\n===================Test #2===================\n";
 
-	// Ñîçäà¸ì îáúåêòû êîìïëåêñíîãî ÷èñëà.
-	Complex ex1_test2(2, 15, "(2 + 15i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex ex2_test2(17, 7, "(17 + 7i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex res_test2; // Âûçâàëñÿ ñòàíäàðòíûé êîíñòðóêòîð.
+	// Ð¡Ð¾Ð·Ð´Ð°Ñ‘Ð¼ Ð¾Ð±ÑŠÐµÐºÑ‚Ñ‹ ÐºÐ¾Ð¼Ð¿Ð»ÐµÐºÑÐ½Ð¾Ð³Ð¾ Ñ‡Ð¸ÑÐ»Ð°.
+	Complex ex1_test2(2, 15, "(2 + 15i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex ex2_test2(17, 7, "(17 + 7i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex res_test2; // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÑÑ‚Ð°Ð½Ð´Ð°Ñ€Ñ‚Ð½Ñ‹Ð¹ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€.
 
-	// Ñëîæåíèå.
+	// Ð¡Ð»Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Summation---\n";
 	res_test2.summarize(ex1_test2, ex2_test2);
 	cout << ex1_test2.getExpression(); cout << " + "; cout << ex2_test2.getExpression(); cout << " = ";
 	res_test2.showResult();
 	res_test2.compare_with(19, 22);
 
-	// Âû÷èòàíèå.
+	// Ð’Ñ‹Ñ‡Ð¸Ñ‚Ð°Ð½Ð¸Ðµ.
 	cout << "---Subtraction---\n";
 	res_test2.subtract(ex1_test2, ex2_test2);
 	cout << ex1_test2.getExpression(); cout << " - "; cout << ex2_test2.getExpression(); cout << " = ";
 	res_test2.showResult();
 	res_test2.compare_with(-15, 8);
 
-	// Äåëåíèå.
+	// Ð”ÐµÐ»ÐµÐ½Ð¸Ðµ.
 	cout << "---Division---\n";
 	res_test2.divide(ex1_test2, ex2_test2);
 	cout << ex1_test2.getExpression(); cout << " / "; cout << ex2_test2.getExpression(); cout << " = ";
 	res_test2.showResult();
 	res_test2.compare_with(0.411, 0.713);
 
-	// Óìíîæåíèå.
+	// Ð£Ð¼Ð½Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Multiplication---\n";
 	res_test2.multiply(ex1_test2, ex2_test2);
 	cout << ex1_test2.getExpression(); cout << " * "; cout << ex2_test2.getExpression(); cout << " = ";
 	res_test2.showResult();
 	res_test2.compare_with(-71, 269);
 
-	// Ñðàâíèâàíèå.
+	// Ð¡Ñ€Ð°Ð²Ð½Ð¸Ð²Ð°Ð½Ð¸Ðµ.
 	cout << "---Comparison---\n";
 	result = compare(ex1_test2, ex2_test2);
 	if (result)
@@ -104,46 +104,46 @@ void Lab1_test2(bool result)
 	}
 
 	res_test2.showNumber();
-} // âûçâàëîñü 3 äåñòðóêòîðà äëÿ îáúåêòîâ: ex1_test2 ; ex2_test2 ; res_test2 . Ïàìÿòü îñâîáîæäåíà.
+} // Ð²Ñ‹Ð·Ð²Ð°Ð»Ð¾ÑÑŒ 3 Ð´ÐµÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€Ð° Ð´Ð»Ñ Ð¾Ð±ÑŠÐµÐºÑ‚Ð¾Ð²: ex1_test2 ; ex2_test2 ; res_test2 . ÐŸÐ°Ð¼ÑÑ‚ÑŒ Ð¾ÑÐ²Ð¾Ð±Ð¾Ð¶Ð´ÐµÐ½Ð°.
 
 void Lab1_test3(bool result)
 {
 	cout << "\n===================Test #3===================\n";
 
-	// Ñîçäà¸ì îáúåêòû êîìïëåêñíîãî ÷èñëà.
-	Complex ex1_test3(18, 16, "(18 + 16i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex ex2_test3(22, 11, "(22 + 11i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex res_test3; // Âûçâàëñÿ ñòàíäàðòíûé êîíñòðóêòîð.
+	// Ð¡Ð¾Ð·Ð´Ð°Ñ‘Ð¼ Ð¾Ð±ÑŠÐµÐºÑ‚Ñ‹ ÐºÐ¾Ð¼Ð¿Ð»ÐµÐºÑÐ½Ð¾Ð³Ð¾ Ñ‡Ð¸ÑÐ»Ð°.
+	Complex ex1_test3(18, 16, "(18 + 16i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex ex2_test3(22, 11, "(22 + 11i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex res_test3; // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÑÑ‚Ð°Ð½Ð´Ð°Ñ€Ñ‚Ð½Ñ‹Ð¹ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€.
 
-	// Ñëîæåíèå.
+	// Ð¡Ð»Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Summation---\n";
 	res_test3.summarize(ex1_test3, ex2_test3);
 	cout << ex1_test3.getExpression(); cout << " + "; cout << ex2_test3.getExpression(); cout << " = ";
 	res_test3.showResult();
 	res_test3.compare_with(40, 27);
 
-	// Âû÷èòàíèå.
+	// Ð’Ñ‹Ñ‡Ð¸Ñ‚Ð°Ð½Ð¸Ðµ.
 	cout << "---Subtraction---\n";
 	res_test3.subtract(ex1_test3, ex2_test3);
 	cout << ex1_test3.getExpression(); cout << " - "; cout << ex2_test3.getExpression(); cout << " = ";
 	res_test3.showResult();
 	res_test3.compare_with(-4, 5);
 
-	// Äåëåíèå.
+	// Ð”ÐµÐ»ÐµÐ½Ð¸Ðµ.
 	cout << "---Division---\n";
 	res_test3.divide(ex1_test3, ex2_test3);
 	cout << ex1_test3.getExpression(); cout << " / "; cout << ex2_test3.getExpression(); cout << " = ";
 	res_test3.showResult();
 	res_test3.compare_with(0.945, 0.255);
 
-	// Óìíîæåíèå.
+	// Ð£Ð¼Ð½Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Multiplication---\n";
 	res_test3.multiply(ex1_test3, ex2_test3);
 	cout << ex1_test3.getExpression(); cout << " * "; cout << ex2_test3.getExpression(); cout << " = ";
 	res_test3.showResult();
 	res_test3.compare_with(220, 550);
 
-	// Ñðàâíèâàíèå.
+	// Ð¡Ñ€Ð°Ð²Ð½Ð¸Ð²Ð°Ð½Ð¸Ðµ.
 	cout << "---Comparison---\n";
 	result = compare(ex1_test3, ex2_test3);
 	if (result)
@@ -156,46 +156,46 @@ void Lab1_test3(bool result)
 	}
 
 	res_test3.showNumber();
-} // âûçâàëîñü 3 äåñòðóêòîðà äëÿ îáúåêòîâ: ex1_test3 ; ex2_test3 ; res_test3 . Ïàìÿòü îñâîáîæäåíà.
+} // Ð²Ñ‹Ð·Ð²Ð°Ð»Ð¾ÑÑŒ 3 Ð´ÐµÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€Ð° Ð´Ð»Ñ Ð¾Ð±ÑŠÐµÐºÑ‚Ð¾Ð²: ex1_test3 ; ex2_test3 ; res_test3 . ÐŸÐ°Ð¼ÑÑ‚ÑŒ Ð¾ÑÐ²Ð¾Ð±Ð¾Ð¶Ð´ÐµÐ½Ð°.
 
 void Lab1_test4(bool result)
 {
 	cout << "\n===================Test #4===================\n";
 
-	// Ñîçäà¸ì îáúåêòû êîìïëåêñíîãî ÷èñëà.
-	Complex ex1_test4(0, 0, "(0 + 0i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex ex2_test4(0, 0, "(0 + 0i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex res_test4; // Âûçâàëñÿ ñòàíäàðòíûé êîíñòðóêòîð.
+	// Ð¡Ð¾Ð·Ð´Ð°Ñ‘Ð¼ Ð¾Ð±ÑŠÐµÐºÑ‚Ñ‹ ÐºÐ¾Ð¼Ð¿Ð»ÐµÐºÑÐ½Ð¾Ð³Ð¾ Ñ‡Ð¸ÑÐ»Ð°.
+	Complex ex1_test4(0, 0, "(0 + 0i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex ex2_test4(0, 0, "(0 + 0i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex res_test4; // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÑÑ‚Ð°Ð½Ð´Ð°Ñ€Ñ‚Ð½Ñ‹Ð¹ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€.
 
-	// Ñëîæåíèå.
+	// Ð¡Ð»Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Summation---\n";
 	res_test4.summarize(ex1_test4, ex2_test4);
 	cout << ex1_test4.getExpression(); cout << " + "; cout << ex2_test4.getExpression(); cout << " = ";
 	res_test4.showResult();
 	res_test4.compare_with(0, 0);
 
-	// Âû÷èòàíèå.
+	// Ð’Ñ‹Ñ‡Ð¸Ñ‚Ð°Ð½Ð¸Ðµ.
 	cout << "---Subtraction---\n";
 	res_test4.subtract(ex1_test4, ex2_test4);
 	cout << ex1_test4.getExpression(); cout << " - "; cout << ex2_test4.getExpression(); cout << " = ";
 	res_test4.showResult();
 	res_test4.compare_with(0, 0);
 
-	// Äåëåíèå.
+	// Ð”ÐµÐ»ÐµÐ½Ð¸Ðµ.
 	cout << "---Division---\n";
 	res_test4.divide(ex1_test4, ex2_test4);
 	cout << ex1_test4.getExpression(); cout << " / "; cout << ex2_test4.getExpression(); cout << " = ";
 	res_test4.showResult();
 	res_test4.compare_with(0, 0);
 
-	// Óìíîæåíèå.
+	// Ð£Ð¼Ð½Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Multiplication---\n";
 	res_test4.multiply(ex1_test4, ex2_test4);
 	cout << ex1_test4.getExpression(); cout << " * "; cout << ex2_test4.getExpression(); cout << " = ";
 	res_test4.showResult();
 	res_test4.compare_with(0, 0);
 
-	// Ñðàâíèâàíèå.
+	// Ð¡Ñ€Ð°Ð²Ð½Ð¸Ð²Ð°Ð½Ð¸Ðµ.
 	cout << "---Comparison---\n";
 	result = compare(ex1_test4, ex2_test4);
 	if (result)
@@ -208,46 +208,46 @@ void Lab1_test4(bool result)
 	}
 
 	res_test4.showNumber();
-} // âûçâàëîñü 3 äåñòðóêòîðà äëÿ îáúåêòîâ: ex1_test4 ; ex2_test4 ; res_test4 . Ïàìÿòü îñâîáîæäåíà.
+} // Ð²Ñ‹Ð·Ð²Ð°Ð»Ð¾ÑÑŒ 3 Ð´ÐµÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€Ð° Ð´Ð»Ñ Ð¾Ð±ÑŠÐµÐºÑ‚Ð¾Ð²: ex1_test4 ; ex2_test4 ; res_test4 . ÐŸÐ°Ð¼ÑÑ‚ÑŒ Ð¾ÑÐ²Ð¾Ð±Ð¾Ð¶Ð´ÐµÐ½Ð°.
 
 void Lab1_test5(bool result)
 {
 	cout << "\n===================Test #5===================\n";
 
-	// Ñîçäà¸ì îáúåêòû êîìïëåêñíîãî ÷èñëà.
-	Complex ex1_test5(-100, 505, "(-100 + 505i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex ex2_test5(190, 707, "(190 + 707i)"); // Âûçâàëñÿ êîíñòðóêòîð ñ ïàðàìåòðàìè.
-	Complex res_test5; // Âûçâàëñÿ ñòàíäàðòíûé êîíñòðóêòîð.
+	// Ð¡Ð¾Ð·Ð´Ð°Ñ‘Ð¼ Ð¾Ð±ÑŠÐµÐºÑ‚Ñ‹ ÐºÐ¾Ð¼Ð¿Ð»ÐµÐºÑÐ½Ð¾Ð³Ð¾ Ñ‡Ð¸ÑÐ»Ð°.
+	Complex ex1_test5(-100, 505, "(-100 + 505i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex ex2_test5(190, 707, "(190 + 707i)"); // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€ Ñ Ð¿Ð°Ñ€Ð°Ð¼ÐµÑ‚Ñ€Ð°Ð¼Ð¸.
+	Complex res_test5; // Ð’Ñ‹Ð·Ð²Ð°Ð»ÑÑ ÑÑ‚Ð°Ð½Ð´Ð°Ñ€Ñ‚Ð½Ñ‹Ð¹ ÐºÐ¾Ð½ÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€.
 
-	// Ñëîæåíèå.
+	// Ð¡Ð»Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Summation---\n";
 	res_test5.summarize(ex1_test5, ex2_test5);
 	cout << ex1_test5.getExpression(); cout << " + "; cout << ex2_test5.getExpression(); cout << " = ";
 	res_test5.showResult();
 	res_test5.compare_with(90, 1212);
 
-	// Âû÷èòàíèå.
+	// Ð’Ñ‹Ñ‡Ð¸Ñ‚Ð°Ð½Ð¸Ðµ.
 	cout << "---Subtraction---\n";
 	res_test5.subtract(ex1_test5, ex2_test5);
 	cout << ex1_test5.getExpression(); cout << " - "; cout << ex2_test5.getExpression(); cout << " = ";
 	res_test5.showResult();
 	res_test5.compare_with(-290, -202);
 
-	// Äåëåíèå.
+	// Ð”ÐµÐ»ÐµÐ½Ð¸Ðµ.
 	cout << "---Division---\n";
 	res_test5.divide(ex1_test5, ex2_test5);
 	cout << ex1_test5.getExpression(); cout << " / "; cout << ex2_test5.getExpression(); cout << " = ";
 	res_test5.showResult();
 	res_test5.compare_with(0.631, 0.311);
 
-	// Óìíîæåíèå.
+	// Ð£Ð¼Ð½Ð¾Ð¶ÐµÐ½Ð¸Ðµ.
 	cout << "---Multiplication---\n";
 	res_test5.multiply(ex1_test5, ex2_test5);
 	cout << ex1_test5.getExpression(); cout << " * "; cout << ex2_test5.getExpression(); cout << " = ";
 	res_test5.showResult();
 	res_test5.compare_with(-376035, 25250);
 
-	// Ñðàâíèâàíèå.
+	// Ð¡Ñ€Ð°Ð²Ð½Ð¸Ð²Ð°Ð½Ð¸Ðµ.
 	cout << "---Comparison---\n";
 	result = compare(ex1_test5, ex2_test5);
 	if (result)
@@ -260,4 +260,4 @@ void Lab1_test5(bool result)
 	}
 
 	res_test5.showNumber();
-} // âûçâàëîñü 3 äåñòðóêòîðà äëÿ îáúåêòîâ: ex1_test5 ; ex2_test5 ; res_test5 . Ïàìÿòü îñâîáîæäåíà.
+} // Ð²Ñ‹Ð·Ð²Ð°Ð»Ð¾ÑÑŒ 3 Ð´ÐµÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€Ð° Ð´Ð»Ñ Ð¾Ð±ÑŠÐµÐºÑ‚Ð¾Ð²: ex1_test5 ; ex2_test5 ; res_test5 . ÐŸÐ°Ð¼ÑÑ‚ÑŒ Ð¾ÑÐ²Ð¾Ð±Ð¾Ð¶Ð´ÐµÐ½Ð°.
